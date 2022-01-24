a921c7be8e5cca13782cec13f38eda33635e0696
minatore06 <stei2002@hotmail.it>
2022-01-24 21:14:59 +0100

    fixed mute time error

--- commands/mute.js
+++ commands/mute.js
@@ -1,3 +1,4 @@
+const ms = require('ms')
 module.exports =
 {
     name: 'mute',
@@ -8,7 +9,7 @@ module.exports =
         var time
         var target
 
-        if(!message.member.permission.has('MODERATE_MEMBERS'))
+        if(!message.member.permissions.has('MODERATE_MEMBERS'))
         {
             return message.reply("you don't have the permissions to use this command")
         }
@@ -23,21 +24,17 @@ module.exports =
             }
         }
 
-        if(!ms(args[1]))
-        {
-            time = '60m'
+        try {   
+            time = ms(args[1])
             args.shift()
-            reason = args.join(' ')
+        } catch (error) {
+            time = ms('60s')
         }
-        else 
-        {
-            time = args[1]
-            args.shift()
-            args.shift()
-            reason = args.join(' ')
-        }
+
+        args.shift()
+        reason = args.join(' ')
         
-        target.timeout(ms(time), reason)
+        target.timeout(time, reason)
         .catch(console.error);
     }
 }
